188794ef28a03f8b71a20bccbfe45822f126d54a
avrahamm <papaabramg@gmail.com>
2025-02-25 13:21:25 +0200

    Adding Slug

--- projects/urls.py
+++ projects/urls.py
@@ -7,5 +7,6 @@ app_name = 'projects'
 urlpatterns = [
     path('', views.all_projects, name='all_projects'),
     path('<int:pk>', views.project_detail, name='project_detail'),
-    path('<slug:slug>', views.project_detail_by_slug, name='project_detail'),
+    path('<slug:slug>', views.project_detail_by_slug,
+         name='project_detail_by_slug'),
 ]
