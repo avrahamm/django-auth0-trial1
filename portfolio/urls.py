016f9ed5e7343dcb0d281d6ce56a6010db3ff700
avrahamm <papaabramg@gmail.com>
2025-02-25 13:33:11 +0200

    Adding empty path route

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.http import HttpResponse
+
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('', lambda request: HttpResponse("Hello"), name="home"),
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
 ]
