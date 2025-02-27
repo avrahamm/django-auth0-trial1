7f64ac4d283c3714cb64b57a86e31b95ac5a253a
avrahamm <papaabramg@gmail.com>
2025-02-27 13:58:29 +0200

    fixing bootstrap requirements.txt

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # my apps
     'projects',
-    # 'bootstrap5',
+    'bootstrap5',
 ]
 
 MIDDLEWARE = [
--- projects/views.py
+++ projects/views.py
@@ -1,26 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required, user_passes_test
-
 from projects.models import Project
 
 
 # Create your views here.
-@login_required
 def all_projects(request):
+    print(f"cur session user =  {request.session.get("user")}")
     # query the db to return all project objects
     projects = Project.objects.all()
     return render(request, 'projects/all_projects.html',
                   {'projects': projects})
 
 
-@login_required
 def project_detail(request, pk):
     project = Project.objects.get(pk=pk)
     return render(request, 'projects/detail.html',
                   {'project': project})
 
 
-@login_required
 def project_detail_by_slug(request, slug):
     # project = Project.objects.get(pk=pk)
     project = Project.objects.get(slug=slug)
